2bf56d4eec0142343f6d902669c84e2af26cd8b4
BianCai <kpmoving@gmail.com>
2018-12-25 17:20:09 +0800

    update

--- docs/genIndex.sh
+++ docs/genIndex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"; pwd
 
-echo "<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>吉祥果博客文章汇总 </title></head><body> <h2>吉祥果博客文章汇总</h2> <p>共 $(find . -size +1500c -name "Post_*" |wc -l) 篇文章，最后更新时间：<em>$(date "+%Y-%m-%d %H:%M:%S" )</em></p>  <ol>" >index.html
+echo "<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>普敬法师博客文章汇总 </title></head><body> <h2>普敬法师博客文章汇总</h2> <p>共 $(find . -size +1500c -name "Post_*" |wc -l) 篇文章，最后更新时间：<em>$(date "+%Y-%m-%d %H:%M:%S" )</em></p>  <ol>" >index.html
 for i in $(find . -size +1500c -name "Post_*" |sort -r)  
 do  
 title=$(head $i| grep "<h2>"|cut -d\> -f2 |cut -d\< -f1)
